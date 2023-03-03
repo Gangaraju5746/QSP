1a8de2d8f707da386e384b9814981de87a25ab0d
Admin <Admin@DESKTOP-4VFU0KO.Dlink>
2023-03-03 17:48:08 +0530

    first myp

---
+++ myproject/src/test/java/com/ganga/AppTest.java
@@ -0,0 +1,19 @@
+package com.ganga;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AppTest {
+   
+	@Test
+	public void testCreatcustomer() {
+		Reporter.log("Creatcustomer",true);
+		
+	}
+	@Test
+	public void testEditcustomer() {
+		Reporter.log("Editcustomer",true);
+	
+	}
+	
+}
